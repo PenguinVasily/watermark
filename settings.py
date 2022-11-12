bfeed908b1942df4d7cbd3e752c24fcabbf3b2c8
Semyon Nikonov <pfrvasily@gmail.com>
2022-11-12 10:16:20 +0300

    init commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+WATERMARK_PATH = './watermark.png'  # Путь к водяному знаку
+
+
+# Режим CENTER (Центр)
+CENTER_WATERMARK_RESIZE_PERCENT = 75  # Процент масштабирования изображения (1 - 100)
+
+
+# Режим FILL (Заполнение)
+FILL_WATERMARK_RESIZE_PERCENT = 25  # Процент масштабирования изображения (0 - 100)
+HORIZONTAL_INDENT_PERCENT = 50  # Горизонтальный отступ для режима FILL (0 - 100)
+VERTICAL_INDENT_PERCENT = 100  # Вертикальный отступ для режима FILL (0 - ...)
